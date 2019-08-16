da3768553488fd688258578bae11428a01eb117d
joellindo <52580005+joellindo@users.noreply.github.com>
2019-08-16 13:04:24 -0400

    Add files via upload

---
+++ assets/data/bagData.ts
@@ -0,0 +1,6 @@
+import {Bag} from '../../app/bag';
+export const MYBAG:Bag[] = [
+    {style:'oversized', material:'leather', color:'black'},
+    {style:'european', material:'fabric', color:'brown'},
+    {style:'carryall', material:'polyester', color:'green'}
+];
